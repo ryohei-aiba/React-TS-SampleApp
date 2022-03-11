a1ddfa8cef814ac6de0b7365ea7d680866b7c9bc
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-12 02:46:33 +0900

    Login user info

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,11 +3,13 @@ import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "./useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
+  const { setLoginUser } = useLoginUser();
 
   const login = useCallback(
     (id: string) => {
@@ -17,18 +19,20 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
             showMessage({ title: "ユーザーが見つかりません", status: "error" });
+            setLoading(false);
           }
         })
         .catch(() => {
           showMessage({ title: "ログインできません", status: "error" });
-        })
-        .finally(() => setLoading(false));
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
