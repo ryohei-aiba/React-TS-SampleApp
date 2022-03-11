90467d0add0ac1ce1f61ba86ff4f0a26d20d1af2
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-12 03:18:24 +0900

    Mordal update

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -19,8 +19,6 @@ export const UserManagement: VFC = memo(() => {
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
 
-  console.log(loginUser);
-
   useEffect(() => {
     getUsers();
   }, [getUsers]);
@@ -54,7 +52,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        onClose={onClose}
+        isAdmin={loginUser?.isAdmin}
+      />
     </>
   );
 });
